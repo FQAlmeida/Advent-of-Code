b4fead9cfaee29849fa92e6a666db29fb81fca86
Otávio Almeida <otavioalmeida650@gmail.com>
2023-12-09 17:47:40 -0300

    part 2

--- 2023/Day_6/boat_race.py
+++ 2023/Day_6/boat_race.py
@@ -3,6 +3,9 @@ from pathlib import Path
 import re
 
 
+# d(t, th) = (t - th) * th
+# th(time, d) => 1*th^2 - t*th + d = 0
+
 def calc_ways_to_win(time: int, distance: int):
     delta = (time**2) - (4 * distance)
     delta_root = delta ** (1 / 2)
@@ -12,26 +15,19 @@ def calc_ways_to_win(time: int, distance: int):
     x1 += 1e-12
     x2 -= 1e-12
     x1, x2 = ceil(x1), floor(x2)
-    print(x1, x2)
     return x2 - x1 + 1
 
-
-# d = (time - time_hold) * time_hold
-# (time - time_hold) * time_hold / d = 1
-# (time - time_hold) / d = 1 / time_hold
-# 1 / d = 1 / time_hold * (time - time_hold)
 
 if __name__ == "__main__":
     with Path("2023/Day_6/data/input.txt").open("r") as fd:
         lines = fd.readlines()
     r = re.compile(r"( )+")
     lines = map(lambda line: line.split(":")[1].strip(), lines)
-    lines = map(lambda line: list(map(int, r.sub(" ", line).split(" "))), lines)
+    lines = map(lambda line: list(map(int, r.sub("", line).split(" "))), lines)
     races = list(zip(*lines))
 
     qtd_ways = 1
     for time, distance in races:
         ways_to_win = calc_ways_to_win(time, distance)
-        print(ways_to_win)
         qtd_ways *= ways_to_win
     print(qtd_ways)
