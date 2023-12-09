5f293658d48997ecc88f58c7d7469d70f145d8f0
Otávio Almeida <otavioalmeida650@gmail.com>
2023-12-09 18:52:56 -0300

    step 2

--- 2023/Day_8/circuit_map.py
+++ 2023/Day_8/circuit_map.py
@@ -1,6 +1,17 @@
+from math import gcd
 from pathlib import Path
 
-from tqdm.auto import tqdm
+
+def get_steps_to_z(origin, locs, dirs):
+    len_dirs = len(dirs)
+    step = 0
+    while not origin.endswith('Z'):
+        next_dir_index = step % len_dirs
+        step += 1
+        next_dir = dirs[next_dir_index]
+        next_dir_s = 0 if next_dir == "L" else 1
+        origin = locs[origin][next_dir_s]
+    return step
 
 
 if __name__ == "__main__":
@@ -9,15 +20,8 @@ if __name__ == "__main__":
     dirs = lines[0].strip()
     locs = {line[:3]: (line[7:10], line.strip()[12:-1]) for line in lines[2:]}
     origins = list(filter(lambda loc: loc.endswith("A"), locs.keys()))
-    len_dirs = len(dirs)
-    step = 0
-    with tqdm(total=1000000000) as pbar:
-        while not all(map(lambda loc: loc.endswith("Z"), origins)):
-            next_dir_index = step % len_dirs
-            step += 1
-            next_dir = dirs[next_dir_index]
-            next_dir_s = 0 if next_dir == 'L' else 1
-            for i, origin in enumerate(origins):
-                origins[i] = locs[origin][next_dir_s]
-            pbar.update(1)
-    print(origins, step)
+    dists = map(lambda origin: get_steps_to_z(origin, locs, dirs), origins)
+    lcm = 1
+    for i in dists:
+        lcm = lcm*i //gcd(lcm, i)
+    print(origins, lcm)
