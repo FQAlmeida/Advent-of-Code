8951a6da4e5d357e669ebecf21cb94dfefebb431
Otávio Almeida <otavioalmeida650@gmail.com>
2023-12-09 19:13:55 -0300

    step 1

---
+++ 2023/Day_9/desert_analyzer.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+
+def fit(readings: list[int]):
+    if not readings:
+        return 0
+    diffs: list[int] = list()
+    for i in range(len(readings[:-1])):
+        diff = readings[i + 1] - readings[i]
+        diffs.append(diff)
+    if all(map(lambda diff: diff == 0, diffs)):
+        return readings[-1]
+    print(*readings)
+    pred = fit(diffs)
+    return readings[-1] + pred
+
+
+if __name__ == "__main__":
+    with Path("2023/Day_9/data/input.txt").open("r") as fd:
+        lines = fd.readlines()
+    data = list(map(lambda line: list(map(int, line.strip().split(" "))), lines))
+    sums = sum(map(fit, data))
+    print(sums)
